91e71c91ccf51f86e53ffd2d204f474860ab05fe
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 20:57:25 -0500

    Completed python algo

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,13 @@
-def factorial(num):
-	# your code here
+def factorial_recur(num):
+	if(num == 1):
+		return 1
+	else:
+		return num * factorial_recur(num-1)
+
+def factorial_iter(num):
+	factor = 1
+
+	for i in range(0, num):
+		factor*=(num-i)
+
+	return factor	
--- python/factorial_spec.py
+++ python/factorial_spec.py
@@ -1,6 +1,10 @@
-from factorial import factorial
+from factorial import factorial_recur, factorial_iter
 
-print(factorial(8) == 40320)
-print(factorial(18) == 6402373705728000)
-print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
+print(factorial_recur(8) == 40320)
+print(factorial_recur(18) == 6402373705728000)
+print(factorial_recur(45) == 119622220865480194561963161495657715064383733760000000000)
+
+print(factorial_iter(8) == 40320)
+print(factorial_iter(18) == 6402373705728000)
+print(factorial_iter(45) == 119622220865480194561963161495657715064383733760000000000)
 # Test how high of a number your program can calculate. Can you push it further?
